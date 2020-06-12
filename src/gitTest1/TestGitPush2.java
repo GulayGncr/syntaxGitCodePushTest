7591ebfdc0c8251ef17f3ff25cbf27f4566f5d5b
GulayGncr <glygencer@gmail.com>
2020-06-11 21:50:47 -0400

    second code push

---
+++ src/gitTest1/TestGitPush2.java
@@ -0,0 +1,24 @@
+package gitTest1;
+
+public class TestGitPush2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean haveMoney=true;
+		boolean resturantIsNear=false;
+		if (haveMoney && resturantIsNear) {
+			
+				System.out.println("I will eat outside");
+		}else {
+			System.out.println("Both condition are not true");
+				
+			}
+		boolean atmIsOpen=true;
+		boolean cashierIsOpen=false;
+		
+		if (atmIsOpen || cashierIsOpen) {
+			System.out.println("I can get cash");
+		}
+	}
+
+}
